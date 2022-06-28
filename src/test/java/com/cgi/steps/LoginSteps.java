bdac34e7afa4f79d52732964fe43ee7a77232c19
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-06-28 18:05:43 +0530

    scenario, scenario outline

--- src/test/java/com/cgi/steps/LoginSteps.java
+++ src/test/java/com/cgi/steps/LoginSteps.java
@@ -2,9 +2,14 @@ package com.cgi.steps;
 
 import java.time.Duration;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -18,37 +23,42 @@ public class LoginSteps {
 	public void i_have_browser_with_orangehrm_application() {
 
 		WebDriverManager.chromedriver().setup();
-		
+
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 
 	}
 
 	@When("I enter username as {string}")
 	public void i_enter_username_as(String username) {
-	    driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
 	}
-
+	
 	@When("I enter password as {string}")
 	public void i_enter_password_as(String password) {
-
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
 	}
 
 	@When("I click on login")
 	public void i_click_on_login() {
-
+		driver.findElement(By.name("Submit")).click();
 	}
 
 	@Then("I should get access to the portal with page content as {string}")
-	public void i_should_get_access_to_the_portal_with_page_content_as(String expectedValue) {
-		System.out.println("then");
+	public void i_should_get_access_to_the_portal_with_page_content_as(String expectedText) {
+		
+		String actualText=driver.findElement(By.partialLinkText("Info")).getText();
+		Assert.assertEquals(expectedText,actualText);
 	}
 
 	@Then("I should get the error message as {string}")
 	public void i_should_get_the_error_message_as(String expectedError) {
 
+		String actualError= driver.findElement(By.id("spanMessage")).getText();
+		
+		Assert.assertEquals(expectedError, actualError);
 	}
 
 }
--- src/test/resources/Feature/Login.feature
+++ src/test/resources/Feature/Login.feature
@@ -10,10 +10,17 @@ Feature: Login
     And I click on login
     Then I should get access to the portal with page content as 'My Info'
 
-  Scenario: Invalid Credential
+  Scenario Outline: Invalid Credential1
     Given I have browser with orangehrm application
-    When I enter username as 'bala'
-    And I enter password as 'bala123'
+    When I enter username as '<username>'
+    And I enter password as '<password>'
     And I click on login
     Then I should get the error message as 'Invalid credentials'
 
+    Examples: 
+      | username | password |
+      | bala     | bala123  |
+      | peter    | peter123 |
+      | mark     | mark123  |
+
+      
